765f038416c0bab48e078e531b444aadb8d92893
Mark Parker <mark@parker-programs.com>
2024-03-16 02:30:39 +0100

    Initial Commit

---
+++ AIDoctor/AIDoctorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIDoctorApp.swift
+//  AIDoctor
+//
+//  Created by Mark Parker on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AIDoctorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
